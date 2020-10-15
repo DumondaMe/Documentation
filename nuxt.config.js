e72ef8c79c1f1b31889c50f283181659fb71ce4e
Grzegorz Leoniec <greg@app-interactive.de>
2020-10-15 22:51:56 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,5 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#00A198'
-  },
-  target: 'static',
-  router: {
-    base: '/Documentation'
   }
 })
